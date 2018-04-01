d0b800a687a2c64a530fa11a2afd6f0887333de1
Arkhane Sagunasingha <arkhane@okstate.edu>
2018-03-31 23:32:45 -0500

    I am trying
    I am trying

--- One_time_Pad.cpp
+++ One_time_Pad.cpp
@@ -1,58 +1,73 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <fstream>
 #include <cmath>
 
 
 using namespace std;
 
-char selection(char m)
-{
-	cout << "Select 'e' as encrypt or 'd' as decrypt" << endl;
-	cin >> m;
 
-	switch (m)
-	{
-	case 'e':
-	case 'E': cout << "You have selected to encrypt file..." << endl;
-		break;
-	case 'd':
-	case 'D': cout << "You have selected to decrypt file..." << endl;
-		break;
-	default: cout << "Invalid entry, please try again...";
-		return selection(m);
-	}return (m);
-}
+
+
 
 void LoadFile()
 {
-	char filename[50];
-	char word[50];
+	char filename[100], word[100], encrypt[100], decrypt[100];
 	char userInput;
+	char menu;
+	int i = 0, sum[100];
 
 	ifstream theFile;
 
 	cout << "Enters a filename level. (ex: level1.txt, level2.txt...)" << endl;
-	cin.getline(filename, 50);
+	cin.getline(filename, 100);
 	theFile.open(filename);
 
 
-	if (!theFile.is_open()) {
+
+	if (!theFile.is_open())
+	{
 		cout << "The filename level you enter is not available." << endl;
 		cout << "Enters an another filename level. (ex: level1.txt, level2.txt...)" << endl;
-		cin.getline(filename, 50);
+		cin.getline(filename, 100);
 		theFile.open(filename);
 	}
 
-	if (theFile.is_open()) {
+	if (theFile.is_open()) // call file 
+	{
 
-		selection(userInput);
+		cout << "Select 'e' as encrypt or 'd' as decrypt" << endl;
+		cin >> menu;
+
+
+		if (menu == 'e' || 'E')
+		{
+			cout << "You have selected to encrypt file..." << endl;
+			cout << "Please type in the message..." << endl;
+			cin.getline(word, 100);
+
+			for (int i = 0; word[i] <= 27; i++)
+			{
+				encrypt[i] = sum[i] + word[i]
+					cout << encrypt[i] << " " << endl;
+
+			}
+
+			// encrypt % 27
+
+
+
+		}
+		if (menu == 'd' || 'D')
+		{
+			cout << "You have selected to encrypt file..." << endl;
+		}
+
+
 
 	}
-	theFile >> word;
-	while (theFile.good()) {
-		cout << word << " ";
 
-	}
 }
 
 int main()
